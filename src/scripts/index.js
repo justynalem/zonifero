4c533a7f4d809228508d7ee8762d4dddf6d6486a
Justyna <lempickajustyna@gmail.com>
2023-09-12 09:16:12 +0200

    add header dropdownMobile

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,92 +1,78 @@
-import 'modern-normalize';
+import Glider from "glider-js";
+import "modern-normalize";
+import "glider-js/glider.css";
 
-import Glider from 'glider-js';
-import 'glider-js/glider.css';
+import "../styles/index.scss";
 
+const calculateSlidesRoShow = () => {
+  const viewportWidth = window.innerWidth;
 
-import '../styles/index.scss';
+  if (viewportWidth <= 768) {
+    return 1;
+  } else if (viewportWidth <= 1290) {
+    return 2;
+  } else {
+    return 4;
+  }
+};
 
-document.addEventListener('DOMContentLoaded', function () {
-
-  const calculateSlidesRoShow = () => {
-    const viewportWidth = window.innerWidth;
-
-    if (viewportWidth <= 768) {
-      return 1;
-    } else if (viewportWidth <= 1290) {
-      return 2;
-    } else {
-      return 4;
-    }
-  };
-
-  const manySlideGlider = new Glider(document.getElementById('manySlideGlider'), {
+document.addEventListener("DOMContentLoaded", function () {
+  new Glider(document.getElementById("manySlideGlider"), {
     slidesToShow: calculateSlidesRoShow(),
-    dots: '.dots',
+    dots: ".dots",
     draggable: true,
   });
 
-
-  const manySlideGlider2 = new Glider(document.getElementById('manySlideGlider2'), {
+  new Glider(document.getElementById("manySlideGlider2"), {
     slidesToShow: calculateSlidesRoShow() / 1.25,
-    dots: '#dots',
+    dots: "#dots",
     draggable: true,
   });
 
-  const oneSlideGlider = new Glider(document.getElementById('oneSlideGlider'), {
+  new Glider(document.getElementById("oneSlideGlider"), {
     slidesToShow: 1,
-    dots: '#dots',
+    dots: "#dots",
     arrows: {
-      prev: '.glider-prev',
-      next: '.glider-next'
-    }
+      prev: ".glider-prev",
+      next: ".glider-next",
+    },
   });
 
-  const oneSlideGliderHero = new Glider(document.getElementById('oneSlideGliderHero'), {
+  new Glider(document.getElementById("oneSlideGliderHero"), {
     slidesToShow: 1,
-    dots: '#dotsHero',
+    dots: "#dotsHero",
     arrows: {
-      prev: '.glider-prev',
-      next: '.glider-next'
-    }
+      prev: ".glider-prev",
+      next: ".glider-next",
+    },
   });
 
+  const accordions = document.getElementsByClassName("accordion");
 
-
-});
-
-
-
-
-
-
-const accordions = document.getElementsByClassName("accordion");
-
-for (const accordion of accordions) {
-  accordion.addEventListener("click", () => {
-    accordion.classList.toggle("accordion__title--active");
-    const panel = accordion.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-  const accordionSections = document.querySelectorAll('.accordion__section');
-
-  accordionSections.forEach((section) => {
-    const header = section.querySelector('.accordion__header');
-    const content = section.querySelector('.accordion__content');
-
-    header.addEventListener('click', () => {
-      section.classList.toggle('open');
-      if (section.classList.contains('open')) {
-        content.style.display = 'block';
+  for (const accordion of accordions) {
+    accordion.addEventListener("click", () => {
+      accordion.classList.toggle("accordion__title--active");
+      const panel = accordion.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
       } else {
-        content.style.display = 'none';
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
+
+  const accordionSections = document.querySelectorAll(".accordion__section");
+
+  accordionSections.forEach(section => {
+    const header = section.querySelector(".accordion__header");
+    const content = section.querySelector(".accordion__content");
+
+    header.addEventListener("click", () => {
+      section.classList.toggle("open");
+      if (section.classList.contains("open")) {
+        content.style.display = "block";
+      } else {
+        content.style.display = "none";
       }
     });
   });
